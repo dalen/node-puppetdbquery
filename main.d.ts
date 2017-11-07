4836d24f9dc0c6cc321c6cd50d985d18641d8421
Erik Dalén <erik.gustav.dalen@gmail.com>
2017-11-07 17:51:17 +0100

    Stricter type for query

--- main.d.ts
+++ main.d.ts
@@ -1,1 +1,3 @@
-export declare function parse(query: string): any[];
+export interface Query extends Array<string | number | boolean | null | Query> {}
+
+export declare function parse(query: string): Query;

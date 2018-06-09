6d1129c4e14be245e442c046a411844005be38d5
Erik Dalén <erik.gustav.dalen@gmail.com>
2018-06-09 22:35:27 +0200

    prettify

--- cli.js
+++ cli.js
@@ -1,30 +1,30 @@
 #!/usr/bin/env node
 /* eslint no-console: "off" */
-const os = require("os");
-const commander = require("commander");
-const puppetdbquery = require("./main");
-const querystring = require("querystring");
-const fs = require("fs");
-const http = require("http");
-const https = require("https");
+const os = require('os');
+const commander = require('commander');
+const puppetdbquery = require('./main');
+const querystring = require('querystring');
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 
 const opts = commander
-  .command("find-nodes [options] <query>")
-  .option("-H, --host <host>", "PuppetDB host")
-  .option("-p, --port <port>", "PuppetDB port", 8080)
-  .option("-s, --ssl", "Use SSL")
+  .command('find-nodes [options] <query>')
+  .option('-H, --host <host>', 'PuppetDB host')
+  .option('-p, --port <port>', 'PuppetDB port', 8080)
+  .option('-s, --ssl', 'Use SSL')
   .option(
-    "--key <keyfile>",
-    "Private SSL key file",
-    `/etc/puppet/ssl/private_keys/${os.hostname()}.pem`
+    '--key <keyfile>',
+    'Private SSL key file',
+    `/etc/puppet/ssl/private_keys/${os.hostname()}.pem`,
   )
   .option(
-    "--cert <certfile>",
-    "SSL certificate file",
-    `/etc/puppet/ssl/certs/${os.hostname()}.pem`
+    '--cert <certfile>',
+    'SSL certificate file',
+    `/etc/puppet/ssl/certs/${os.hostname()}.pem`,
   )
-  .option("--ca <cafile>", "SSL CA certificate file", "/etc/puppet/ssl/ca.pem")
-  .option("-P, --print", "Print parsed query and exit")
+  .option('--ca <cafile>', 'SSL CA certificate file', '/etc/puppet/ssl/ca.pem')
+  .option('-P, --print', 'Print parsed query and exit')
   .parse(process.argv);
 
 let query;
@@ -45,8 +45,8 @@ if (opts.print) {
     port: opts.port,
     path: `/pdb/query/v4/nodes?${querystring.stringify({ query })}`,
     headers: {
-      Accept: "application/json"
-    }
+      Accept: 'application/json',
+    },
   };
   let httplib;
   if (opts.ssl) {
@@ -61,17 +61,17 @@ if (opts.print) {
   // We have the full response, parse it and print the node names
   httplib
     .get(options, res => {
-      let data = "";
-      res.on("data", chunk => {
+      let data = '';
+      res.on('data', chunk => {
         data += chunk;
       });
 
-      return res.on("end", () =>
-        JSON.parse(data).forEach(node => console.log(node.certname))
+      return res.on('end', () =>
+        JSON.parse(data).forEach(node => console.log(node.certname)),
       );
     })
 
-    .on("error", e =>
-      console.log(`Error fetching from ${opts.host} : ${opts.port} ${e}`)
+    .on('error', e =>
+      console.log(`Error fetching from ${opts.host} : ${opts.port} ${e}`),
     );
 }

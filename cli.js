780f9a51025cbeb4da710607229bcf87213c0daf
Erik Dalén <erik.gustav.dalen@gmail.com>
2018-06-09 22:25:46 +0200

    Switch from nomnom to commander

--- cli.js
+++ cli.js
@@ -1,80 +1,37 @@
 #!/usr/bin/env node
 /* eslint no-console: "off" */
-const os = require('os');
-const nomnom = require('nomnom');
-const puppetdbquery = require('./main');
-const querystring = require('querystring');
-const fs = require('fs');
-const http = require('http');
-const https = require('https');
+const os = require("os");
+const commander = require("commander");
+const puppetdbquery = require("./main");
+const querystring = require("querystring");
+const fs = require("fs");
+const http = require("http");
+const https = require("https");
 
-const getCommandlineOptions = () => {
-  const opts = nomnom.script('find-nodes')
-    .option('host', {
-      abbr: 'H',
-      default: 'puppetdb',
-      metavar: 'HOST',
-      help: 'PuppetDB host',
-    })
-    .option('port', {
-      abbr: 'p',
-      default: 8080,
-      metavar: 'PORT',
-      help: 'PuppetDB port',
-      callback(port) {
-        return parseInt(port, 10);
-      },
-    })
-    .option('ssl', {
-      abbr: 's',
-      flag: true,
-      default: false,
-      help: 'Use SSL',
-    })
-    .option('key', {
-      default: `/etc/puppet/ssl/private_keys/${os.hostname()}.pem`,
-      metavar: 'KEY',
-      help: 'Private SSL key file',
-    })
-    .option('cert', {
-      default: `/etc/puppet/ssl/certs/${os.hostname()}.pem`,
-      metavar: 'CERT',
-      help: 'SSL certificate file',
-    })
-    .option('ca', {
-      default: '/etc/puppet/ssl/ca.pem',
-      metavar: 'CACERT',
-      help: 'SSL CA certificate file',
-    })
-    .option('version', {
-      abbr: 'v',
-      flag: true,
-      help: 'print version and exit',
-      callback() {
-        return process.env.npm_package_version;
-      },
-    })
-    .option('print', {
-      abbr: 'P',
-      flag: true,
-      default: false,
-      help: 'Print parsed query and exit',
-    })
-    .option('query', {
-      flag: true,
-      required: true,
-      position: 0,
-      help: 'query string',
-    })
-    .parse();
-  return opts;
-};
+const opts = commander
+  .command("find-nodes [options] <query>")
+  .option("-H, --host <host>", "PuppetDB host")
+  .option("-p, --port <port>", "PuppetDB port", 8080)
+  .option("-s, --ssl", "Use SSL")
+  .option(
+    "--key <keyfile>",
+    "Private SSL key file",
+    `/etc/puppet/ssl/private_keys/${os.hostname()}.pem`
+  )
+  .option(
+    "--cert <certfile>",
+    "SSL certificate file",
+    `/etc/puppet/ssl/certs/${os.hostname()}.pem`
+  )
+  .option("--ca <cafile>", "SSL CA certificate file", "/etc/puppet/ssl/ca.pem")
+  .option("-P, --print", "Print parsed query and exit")
+  .parse(process.argv);
 
-const opts = getCommandlineOptions();
 let query;
 try {
-  query = JSON.stringify(puppetdbquery.parse(opts.query));
+  query = JSON.stringify(puppetdbquery.parse(opts.args[0]));
 } catch (err) {
+  console.log(opts);
   console.log(err);
 }
 
@@ -88,8 +45,8 @@ if (opts.print) {
     port: opts.port,
     path: `/pdb/query/v4/nodes?${querystring.stringify({ query })}`,
     headers: {
-      Accept: 'application/json',
-    },
+      Accept: "application/json"
+    }
   };
   let httplib;
   if (opts.ssl) {
@@ -102,14 +59,19 @@ if (opts.print) {
   }
 
   // We have the full response, parse it and print the node names
-  httplib.get(options, (res) => {
-    let data = '';
-    res.on('data', (chunk) => { data += chunk; });
+  httplib
+    .get(options, res => {
+      let data = "";
+      res.on("data", chunk => {
+        data += chunk;
+      });
 
-    return res.on('end', () =>
-      JSON.parse(data).forEach(node => console.log(node.certname))
+      return res.on("end", () =>
+        JSON.parse(data).forEach(node => console.log(node.certname))
+      );
+    })
+
+    .on("error", e =>
+      console.log(`Error fetching from ${opts.host} : ${opts.port} ${e}`)
     );
-  })
-
-  .on('error', e => console.log(`Error fetching from ${opts.host} : ${opts.port} ${e}`));
 }
